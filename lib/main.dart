43e02a21aab9d8983829b59242815085d1b702db
rodrigogaleano <rodrigogaleano@jera.com.br>
2023-09-11 20:11:10 -0400

    [UPDATE] Add app theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/localization.dart';
 
 import 'router/mobile_router.dart';
+import 'support/style/app_themes.dart';
 import 'support/utils/localize.dart';
 
 void main() {
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: AppThemes.theme,
       routes: MobileRouter.routes,
       initialRoute: MobileRouter.initialRoute,
       supportedLocales: Localization.supportedLocales,
